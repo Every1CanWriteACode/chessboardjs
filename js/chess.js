a1e8c6a0485e5f84c0f541dbbe2d66605eaef639
Every1CanWriteACode <61137980+Every1CanWriteACode@users.noreply.github.com>
2020-03-09 12:56:44 +0200

    For cycle changes

--- js/chess.js
+++ js/chess.js
@@ -3,30 +3,36 @@
 const board = document.querySelector('.board');
 
 console.log(board);
-const rowsCount = 4;
-const columnsCount = 4;
+const rowsCount = 6;
+const columnsCount = 6;
 const firstColor = '#fff';
 const secondColor = '#000';
+let color = '';
+
 let HTML = '';
 board.innerHTML = HTML;
 
 for( let y=1; y<=rowsCount; y++){
 
-    HTML = `<div class="row">`;
-
+    HTML += `<div class="row"
+                  style="height: calc(100% / ${rowsCount});">`;  
         for ( let x=1; x<=columnsCount; x++){
 
+            if ( (x+y)%2 === 0 ){
+                color = firstColor;
+            } else {
+                color = secondColor;
+            }
             HTML += `<div class="cell"
-                    style="background-color: red;>
-                    [${x}, ${y}]
+                          style="background-color: ${color};
+                                 width: calc(100% / ${columnsCount});">
                     </div>`;
             
         }
-
-    HTML += `</div>`;
-    board.innerHTML += HTML;
-
+    HTML += `</div>`    
 }
+
+board.innerHTML = HTML;
 
 // const someThingNew = `llvfghjnbvghjsd ${HTML} ${19+87} CDCD`
 // let HTML = '<div class="row">row1</div>';
